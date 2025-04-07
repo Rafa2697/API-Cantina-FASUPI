022324a25f1ac1c8a4094defcb18228442e2e303
Rafael <Rafael.mattos468@gmail.com>
2025-04-07 13:04:48 -0300

    fix: atualiza a configuração da porta do servidor e desativa a chamada do app.listen

--- dist/server.js
+++ dist/server.js
@@ -7,7 +7,7 @@ import routerOrder from './routes/order.routes';
 import routerOrderItem from './routes/orderItem.routes';
 import rootRouter from './routes/root.routes';
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // Define a porta do servidor, padrão 3000
 dotenv.config(); // Carrega as variáveis de ambiente do .env
 app.use(express.json()); // Middleware para analisar JSON no corpo das requisições
 app.use(rootRouter); // Usando as rotas definidas no arquivo root.routes.ts
@@ -16,8 +16,8 @@ app.use(routeFood);
 app.use(routeCategory); // Usando as rotas definidas no arquivo category.routes.ts
 app.use(routerOrder); // Usando as rotas definidas no arquivo order.routes.ts
 app.use(routerOrderItem); // Usando as rotas definidas no arquivo orderItem.routes.ts
-app.listen(PORT, () => {
-    console.log(`servidor rodando na porta ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`servidor rodando na porta ${PORT}`);
+// })
 export default app; // Exporta o app para ser usado em testes ou outros módulos
 //# sourceMappingURL=server.js.map
